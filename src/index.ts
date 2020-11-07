36db28c1d313ba84427887a703934d20bae46596
william341 <wwokwilliam@gmail.com>
2020-11-07 07:30:51 -0700

    update starship-schema version

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ const sysInfo = {
   serverName: "starship-server",
   version: "prealpha",
   production: !Boolean(process.env.DEVELOPMENT),
-  schemaVersion: "0.1c",
+  schemaVersion: "0.2",
   syncEnabled: process.env.REDIS_URL !== undefined,
   subscriptionsSupported: false,
   supportedFeatures: ["users", "reports"],
