5d2f21cd1d1468829f09dce6d6d7d4f7a2df7979
william341 <wwokwilliam@gmail.com>
2021-08-24 13:32:07 -0700

    files and forums documentation

--- src/database/components/files/Files.ts
+++ src/database/components/files/Files.ts
@@ -3,10 +3,15 @@ import nanoIdPlugin from "mongoose-nanoid";
 import { IComponent } from "../IComponent";
 
 export interface IFiles extends IComponent {
+  /** The file component's ID. */
   _id: string,
+  /** The creation date of the file component. */
   createdAt: Date,
+  /** The ID of the user who created the file component. */
   owner: string,
+  /** The last update date of the file component. */
   updatedAt: Date,
+  /** The ID of the planet the file component belongs to. */
   planet: string
 }
 
--- src/database/components/forum/ForumReplies.ts
+++ src/database/components/forum/ForumReplies.ts
@@ -2,16 +2,27 @@ import {model, Schema, Document} from "mongoose";
 import nanoIdPlugin from "mongoose-nanoid";
 
 export interface IForumReply extends Document {
+  /** The reply's ID. */
   _id: string,
+  /** The ID of the post that this reply is a reply to. */
   postId: string,
+  /** The ID of the forum this reply is in. */
   componentId: string,
+  /** The content of the reply. */
   content: string,
+  /** The ID of the user that created the reply. */
   owner: string,
+  /** The ID of the planet that the reply is in. */
   planet: string,
+  /** The reactions to this reply. */
   reactions: [{emoji: string, reactors: string[]}],
+  /** Whether or not this reply is stickied. This field is currently unused. */
   stickied: boolean,
+  /** The date the reply was created. */
   createdAt: Date,
+  /** The date the reply was last modified. */
   updatedAt: Date,
+  /** The IDs of the mentioned users. */
   mentions: [string]
 }
 
